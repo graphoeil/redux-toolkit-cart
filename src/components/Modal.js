4b755d7aa162e0b52986130989b0da64a193d4b2
graphoeil <graphoeil@gmail.com>
2022-08-17 19:17:44 +0200

    Minor changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,7 +17,7 @@ const Modal = () => {
 				<h4>Remove all items from your shopping cart ?</h4>
 				<div className="btn-container">
 					<button type="button" className="btn confirm-btn" 
-						onClick={ () => { dispatch(clearCart()); dispatch(closeModal()); dispatch(getCartItems('smartphone')); } }>
+						onClick={ () => { dispatch(clearCart()); dispatch(closeModal()); } }>
 						Confirm
 					</button>
 					<button type="button" className="btn clear-btn" 
